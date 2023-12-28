e13541ee009fcbcef3faad4d621ded409163bfda
Anand Shukla <anandshukla.web@gmail.com>
2023-12-28 22:36:19 +0530

    Updated About page

--- src/components/DetailBanner.js
+++ src/components/DetailBanner.js
@@ -4,15 +4,9 @@ import Banner from "../assets/images/slider/Banner.jpg";
 const DetailBanner = () => {
   return (
     <>
-      <div
-        id="carouselExampleIndicators"
-        className="carousel slide detail-banner"
-        data-ride="carousel"
-      >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={Banner} className="d-block w-100" alt="..." />
-          </div>
+      <div className="h-96">
+        <div className="">
+          <img src={Banner} className=" w-100 h-96 object-cover" alt="..." />
         </div>
       </div>
     </>
