e4457576c974ba15bf7082d5165449293bf673f8
Anand Shukla <anandshukla.web@gmail.com>
2023-12-28 20:50:20 +0530

    Added Tailwind and updated Navbar component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./index.css";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,43 +5,31 @@ import Logo from "../assets/images/Logo.png";
 const Navbar = () => {
   return (
     <>
-      <div className="container-fluid">
-        <nav className="navbar navbar-expand-lg navbar-light ">
-          <Link className="navbar-brand" to="/">
-            <img src={Logo} alt="Logo" className="logo" />
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarTogglerDemo02"
-            aria-controls="navbarTogglerDemo02"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className=" navbar-nav ml-auto">
-              <li className="nav-item active">
-                <Link to="/" className="nav-link">
+      <div className="container shadow-lg">
+        <div className="flex justify-between items-center py-2">
+          <div>
+            <img src={Logo} className="w-20" alt="Fitness App" />
+          </div>
+          <div>
+            <ul className="flex gap-8">
+              <li>
+                <Link to="/"  className="text-gray-700 hover:text-teal-400 hover:no-underline">
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#search-exercise">
+              <li>
+                <a className="text-gray-700 hover:text-teal-400 hover:no-underline" href="#search-exercise">
                   Exercises
                 </a>
               </li>
-              <li className="nav-item">
-                <Link to="/about" className="nav-link">
+              <li >
+                <Link to="/about"  className="text-gray-700 hover:text-teal-400 hover:no-underline">
                   About
                 </Link>
               </li>
             </ul>
           </div>
-        </nav>
+        </div>
       </div>
     </>
   );
